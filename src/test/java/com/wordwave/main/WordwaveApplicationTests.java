c9cf5a9c8d4eddca53d009e173e19ae0dbcb6b24
AJN <buzz781@gmail.com>
2023-12-05 14:47:17 +0900

    feat: 초기 프로젝트 생성

---
+++ src/test/java/com/wordwave/main/WordwaveApplicationTests.java
@@ -0,0 +1,13 @@
+package com.wordwave.main;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WordwaveApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
