d25339a09f634bc1345466a5e1bbe5556be20602
AJN <buzz781@gmail.com>
2023-12-09 16:19:34 +0900

    feat: vocabook 엔티티 생성

--- src/main/java/com/wordwave/vocabook/Vocabook.java
+++ src/main/java/com/wordwave/vocabook/Vocabook.java
@@ -1,5 +1,25 @@
 package com.wordwave.vocabook;
 
-public class Vocabook {
+import java.util.List;
 
+import com.wordwave.voca.Voca;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.OneToMany;
+import lombok.Getter;
+
+@Entity
+@Getter
+public class Vocabook {
+	
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private long id;
+	
+	@OneToMany(mappedBy = "vocabook")
+	private List<Voca> vocas;
+	
 }
