8fb2dc50c32394d6b21d3965e4b0efc4d691928c
AJN <buzz781@gmail.com>
2024-01-02 14:27:27 +0900

    fix: Voca 엔티티 구조 수정

--- src/main/java/com/wordwave/voca/Voca.java
+++ src/main/java/com/wordwave/voca/Voca.java
@@ -14,19 +14,22 @@ public class Voca {
 	
 	@Id
 	@GeneratedValue(strategy = GenerationType.IDENTITY)
-	private long id;
+	private Long id;
 	
 	// TODO:korQuestion ,engQuestion 중 랜덤으로 
 	
-	//ex) Q: 사과 - A: apple
-	private String korQuestion;
-	private String korAnswer;
+	//ex) Q: 사과 - A: 구글번역기 API를 사용한 사용자의 입력값 번역이 맞아 떨어질경우
+	//TODO: KOR문제일 경우 영어 포함안되게 해야함.
+	private String korWord;
 	
-	//ex) Q: apple - A: 사과
-	private String engQuestion;
-	private String engAnswer;
+	//ex) Q: apple - A: 구글번역기 API를 사용한 사용자의 입력값 번역이 맞아 떨어질경우
+	//TODO: ENG문제일 경우 한국어 포함안되게 해야함.
+	private String engWord;
+
+	//ex: 채용, 법률 (챕터느낌) - 해커스토익보카
+	private String category;
 	
-	@ManyToOne(optional = false)
+	@ManyToOne
 	private VocaBook vocaBook;
 	
 }
