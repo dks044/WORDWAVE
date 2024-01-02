13028b518a5b2474efe51e0f8a4edc429b6a3579
AJN <buzz781@gmail.com>
2024-01-02 16:29:49 +0900

    feat: VocaRepository 생성

---
+++ src/main/java/com/wordwave/voca/VocaRepository.java
@@ -0,0 +1,9 @@
+package com.wordwave.voca;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+interface VocaRepository extends JpaRepository<Voca, Long>{
+	
+}
