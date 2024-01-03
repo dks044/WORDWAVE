51eff6982f6833a41181adbf27f23f4938e34402
ddolboghi <rhdfyd128@gmail.com>
2024-01-03 18:39:24 +0900

    test: GrammarBook 조회 기능 테스트

---
+++ src/test/java/com/wordwave/grammarbook/GrammarBookServiceTest.java
@@ -0,0 +1,27 @@
+package com.wordwave.grammarbook;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.transaction.annotation.Transactional;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+@SpringBootTest
+@Transactional
+//@TestPropertySource(locations = "classpath:application-test.properties")
+class GrammarBookServiceTest {
+    @Autowired
+    GrammarBookService grammarBookService;
+
+    @Test
+    @DisplayName("GrammarBook id로 GrammarBook을 조회한다.")
+    void grammarBookSaveTest() {
+        Long id = 4L;
+
+        GrammarBookDto grammarBookDto = grammarBookService.getGrammarBookById(id);
+
+        assertThat(grammarBookDto.getName()).isEqualTo("test book");
+    }
+}
