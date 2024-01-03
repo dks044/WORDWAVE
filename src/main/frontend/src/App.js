576eb38cf88ebe4940a9750f4d627c71a24f3768
AJN <buzz781@gmail.com>
2024-01-03 18:35:22 +0900

    chore: React+Spring 연동 설정 완료

--- src/main/frontend/src/App.js
+++ src/main/frontend/src/App.js
@@ -2,17 +2,17 @@ import React, {useEffect, useState} from 'react';
 import axios from 'axios';
 
 function App() {
-  //  const [hello, setHello] = useState('')
+   const [main, setMain] = useState('')
 
-  //   useEffect(() => {
-  //       axios.get('/api/hello')
-  //       .then(response => setHello(response.data))
-  //       .catch(error => console.log(error))
-  //   }, []);
+    useEffect(() => {
+        axios.get('/api')
+        .then(response => setMain(response.data))
+        .catch(error => console.log(error))
+    }, []);
 
     return (
         <div>
-            백엔드에서 가져온 데이터입니다 : 
+            테스트 {main}
         </div>
     );
 }
---
+++ src/main/java/com/wordwave/main/MainController.java
@@ -0,0 +1,14 @@
+package com.wordwave.main;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class MainController {
+	
+	@GetMapping("/api")
+	public String mainPage() {
+		return "mainPage";
+	}
+	
+}
