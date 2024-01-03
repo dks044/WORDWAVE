caf0a3d045e4ea7a216dcdb42d68574b5f826935
ddolboghi <rhdfyd128@gmail.com>
2024-01-03 18:03:41 +0900

    fix: GrammarBook 조회 기능 수정

--- src/main/java/com/wordwave/grammarbook/GrammarBookService.java
+++ src/main/java/com/wordwave/grammarbook/GrammarBookService.java
@@ -1,31 +1,26 @@
 package com.wordwave.grammarbook;
 
+import com.wordwave.exception.DataNotFoundException;
 import com.wordwave.grammar.GrammarDto;
+import com.wordwave.grammar.GrammarRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
-import org.springframework.transaction.annotation.Transactional;
-
-import java.util.Optional;
 
 @Service
 @RequiredArgsConstructor
 public class GrammarBookService {
     private final GrammarBookRepository grammarBookRepository;
+    private final GrammarRepository grammarRepository;
+
     public GrammarBookDto getGrammarBookById(Long id) {
-        Optional<GrammarBook> grammarBook = this.grammarBookRepository.findById(id);
-        if (grammarBook.isEmpty()) {
-            throw new DataNotFoundException("Grammar book not found.");
-        }
+        GrammarBook grammarBook = this.grammarBookRepository.findById(id)
+                .orElseThrow(() -> new DataNotFoundException("Grammar book not found."));
+
         GrammarBookDto grammarBookDto = new GrammarBookDto();
-        grammarBookDto.setName(grammarBook.get().getName());
-        grammarBookDto.setGrammars(grammarBook.get().getGrammars().stream()
-                .map(grammar -> new GrammarDto(grammar.getSentence(), grammar.getGrammarBook().getName()))
+        grammarBookDto.setName(grammarBook.getName());
+        grammarBookDto.setGrammarDtos(grammarBook.getGrammars().stream()
+                .map(grammar -> new GrammarDto(grammar.getSentence(), grammarBook.getName()))
                 .toList());
         return grammarBookDto;
-    }
-
-    @Transactional
-    public void createGrammarBook(GrammarBookDto grammarBookDto) {
-        //TODO: grammarBookDto를 grammarBook으로 변환해서 저장하기
     }
 }
