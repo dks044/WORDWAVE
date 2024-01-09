81a1fb7035599f3b1a03315c12da84900b94f01f
ddolboghi <rhdfyd128@gmail.com>
2024-01-09 17:41:56 +0900

    test: 새 GrammarBook 데이터 생성

--- src/test/java/com/wordwave/grammarbook/GrammarBookRepositoryTest.java
+++ src/test/java/com/wordwave/grammarbook/GrammarBookRepositoryTest.java
@@ -17,10 +17,10 @@ class GrammarBookRepositoryTest {
     @Test
     @Rollback(false)
     void cascadePersistTest() {
-        GrammarBook grammarBook = GrammarBook.builder().name("test book").build();
-        Grammar grammar1 = Grammar.builder().sentence("test sentence1").grammarBook(grammarBook).build();
-        Grammar grammar2 = Grammar.builder().sentence("test sentence2").grammarBook(grammarBook).build();
-        Grammar grammar3 = Grammar.builder().sentence("test sentence3").grammarBook(grammarBook).build();
+        GrammarBook grammarBook = GrammarBook.builder().name("test book2").build();
+        Grammar grammar1 = Grammar.builder().sentence("test sentence4").grammarBook(grammarBook).build();
+        Grammar grammar2 = Grammar.builder().sentence("test sentence5").grammarBook(grammarBook).build();
+        Grammar grammar3 = Grammar.builder().sentence("test sentence6").grammarBook(grammarBook).build();
 
         grammarBook.getGrammars().addAll(Lists.newArrayList(grammar1, grammar2, grammar3));
 
