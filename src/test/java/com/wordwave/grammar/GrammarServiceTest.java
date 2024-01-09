2c57674292876f2c3608820a4cf8d23360754ac1
ddolboghi <rhdfyd128@gmail.com>
2024-01-09 17:45:36 +0900

    test: Grammar 조회 테스트 코드 작성

--- src/test/java/com/wordwave/grammar/GrammarServiceTest.java
+++ src/test/java/com/wordwave/grammar/GrammarServiceTest.java
@@ -4,11 +4,18 @@ import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.transaction.annotation.Transactional;
+
+import static org.assertj.core.api.Assertions.assertThat;
 
 @SpringBootTest
+@Transactional
 class GrammarServiceTest {
     @Autowired
     GrammarService grammarService;
+
+    @Autowired
+    GrammarRepository grammarRepository;
 
     @Test
     @DisplayName("새로운 GrammarDto를 저장한다.")
@@ -21,5 +28,15 @@ class GrammarServiceTest {
         grammarService.createGrammar(grammarDto1);
         grammarService.createGrammar(grammarDto2);
         grammarService.createGrammar(grammarDto3);
+    }
+
+    @Test
+    @DisplayName("Grammar id로 Grammar를 조회한다.")
+    void getGrammarTest() {
+        Long id = 15L;
+
+        GrammarDto grammarDto = this.grammarService.getGrammar(id);
+
+        assertThat(grammarDto.getGrammarBookName()).isEqualTo("test book");
     }
 }
