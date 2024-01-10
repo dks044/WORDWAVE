4a016ff68ff0829e049d80c025d180055484e25c
AJN <buzz781@gmail.com>
2024-01-10 18:07:29 +0900

    feat: 다이얼 메뉴 애니메이션 구현

--- src/main/frontend/src/components/DialMenu.js
+++ src/main/frontend/src/components/DialMenu.js
@@ -1,36 +1,62 @@
-import React from "react";
-import styled from "styled-components";
+import React, { useState } from "react";
+import styled, { css } from "styled-components";
 import { TiWaves } from "react-icons/ti";
 
 const CircleButton = styled.button`
     //색 설정
-    background: grey;
+    background: skyblue;
     color: white;
     //사이즈 설정
-    width: 80px;
-    height: 80px;
+    width: 100px;
+    height: 100px;
     border-radius: 50%;
     //포지션 설정
     z-index: 5;
     position: absolute;
-    bottom: -20px;
+    bottom: -30px;
     left: 50%;
-    transform: translateX(-50%); /* Add this line */
+    transform: translateX(-50%);
     align-items: center;
     justify-content: center;
     //테두리 설정
-    //border: 1px solid #E7E7E7;
     border: none;
     outline: none;
-
+    //이벤트 설정
+    cursor: pointer;
+    &:hover {
+        background: #63e6be;
+    }
+    &:active {
+        background: #20c997;
+    }
+    //애니메이션 설정
+    transition: 0.325s all ease-in;
+    //사용자정의 이벤트 설정
+    ${props =>
+        props.open &&
+        css`
+        background: #2E2EFD;
+        &:hover {
+            background: #5783FB;
+        }
+        &:active {
+            background: #003ADF;
+        }
+        transform: translateX(-50%) translateY(0%) rotate(360deg);
+    `}
 `
 
 
 function DialMenu(){
+    const [open,setOpen] = useState(false);
+    const onToggle = () =>{
+        setOpen(!open);
+        console.log(open);
+    };
     
     return (
-        <CircleButton>
-            <TiWaves size={65}/>
+        <CircleButton onClick={onToggle} open={open}>
+            <TiWaves size={85}/>
         </CircleButton>
     );
 
