1a19b0888ece6e6e592a39945e4f1ec5d6ee25de
AJN <buzz781@gmail.com>
2024-01-10 14:09:57 +0900

    delete: 구현한 nav 삭제

--- src/main/frontend/src/App.js
+++ src/main/frontend/src/App.js
@@ -1,24 +1,15 @@
 import React, {useEffect, useState} from 'react';
 import axios from 'axios';
 import 'bootstrap/dist/css/bootstrap.min.css';
-import Navigation from './component/nav';
-import { Route, Routes } from 'react-router-dom';
+
 
 
 function App() {
-   const [main, setMain] = useState('')
 
-    useEffect(() => {
-        axios.get('/api')
-        .then(response => setMain(response.data))
-        .catch(error => console.log(error))
-    }, []);
 
     return (
         <>
-            <Routes>
-                <Route></Route>
-            </Routes>
+
         </>
     );
 }
