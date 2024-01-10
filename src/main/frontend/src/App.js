a7b1cf29a49d34e7dc6f80036a90b0d91c0b7c80
AJN <buzz781@gmail.com>
2024-01-10 14:29:58 +0900

    feat: 배경색 설정

--- src/main/frontend/src/App.js
+++ src/main/frontend/src/App.js
@@ -1,15 +1,24 @@
 import React, {useEffect, useState} from 'react';
 import axios from 'axios';
 import 'bootstrap/dist/css/bootstrap.min.css';
+import { createGlobalStyle } from 'styled-components';
 
 
 
 function App() {
+    const GlobalStyle = createGlobalStyle`
+        body {
+            background: linear-gradient(to right, #89CFF0 0%, #B2FFFF 50%, #89CFF0 100%);
+        }
+    `
+
+
 
 
     return (
         <>
-
+            <GlobalStyle />
+            배경화면 입니다~
         </>
     );
 }
