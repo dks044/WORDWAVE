6284371dc5d3c87191284fc0830be11bac4772d2
ddolboghi <rhdfyd128@gmail.com>
2024-01-10 16:21:02 +0900

    feat: Grammar 저장 기능 추가

--- src/main/java/com/wordwave/grammarbook/GrammarBookService.java
+++ src/main/java/com/wordwave/grammarbook/GrammarBookService.java
@@ -1,10 +1,12 @@
 package com.wordwave.grammarbook;
 
 import com.wordwave.exception.DataNotFoundException;
+import com.wordwave.grammar.Grammar;
 import com.wordwave.grammar.GrammarDto;
 import com.wordwave.grammar.GrammarRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 @Service
 @RequiredArgsConstructor
@@ -22,5 +24,23 @@ public class GrammarBookService {
                 .map(grammar -> new GrammarDto(grammar.getSentence(), grammarBook.getName()))
                 .toList());
         return grammarBookDto;
+    }
+
+    private GrammarBook createGrammarBook(String name) {
+        return this.grammarBookRepository.save(GrammarBook.builder().name(name).build());
+    }
+
+    @Transactional
+    public void saveGrammar(GrammarDto grammarDto) {
+        GrammarBook grammarBook = this.grammarBookRepository.findByName(grammarDto.getGrammarBookName())
+                .orElseGet(() -> createGrammarBook(grammarDto.getGrammarBookName()));
+
+        Grammar grammar = Grammar.builder()
+                .sentence(grammarDto.getSentence())
+                .grammarBook(grammarBook)
+                .build();
+
+        grammarBook.addGrammar(grammar);
+        this.grammarRepository.save(grammar);
     }
 }
