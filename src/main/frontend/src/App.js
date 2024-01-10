49aa1db29aa7cf70a1816ee84104b4a21b7019e1
AJN <buzz781@gmail.com>
2024-01-10 16:23:22 +0900

    chore&feat: 설치한 라이브러리 frontend 경로로 이동 및 라우팅 처리 구현

--- src/main/frontend/src/App.js
+++ src/main/frontend/src/App.js
@@ -1,31 +1,16 @@
 import React, {useEffect, useState} from 'react';
 import axios from 'axios';
-import { createGlobalStyle } from 'styled-components';
-import GmarketSansTTFLight from './fonts/GmarketSansTTFLight.ttf';
+import Layout from './components/Layout';
+import Home from './pages/Home';
+import { Route,Routes } from 'react-router-dom';
 
 function App() {
-    const GlobalStyle = createGlobalStyle`
-        @font-face {
-            font-family: 'GmarketSansTTFLight';
-            src: url(${GmarketSansTTFLight}) format('truetype');
-            font-weight: normal;
-            font-style: normal;
-        }
-
-        body {
-            font-family: 'GmarketSansTTFLight', sans-serif;
-            background: linear-gradient(to right, #89CFF0 0%, #B2FFFF 50%, #89CFF0 100%);
-        }
-    `;
-
-
-
-
     return (
-        <>
-            <GlobalStyle />
-            <h1>WORDWAVE 입니다, 반갑습니다 ENGLISH 공부 페이지 입니다</h1>
-        </>
+        <Routes>
+            <Route path='/' element={<Layout/>}>
+                <Route index element={<Home />} />
+            </Route>
+        </Routes>
     );
 }
 
--- src/main/frontend/src/index.js
+++ src/main/frontend/src/index.js
@@ -3,12 +3,30 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import { BrowserRouter } from 'react-router-dom';
+import { createGlobalStyle } from 'styled-components';
+import GmarketSansTTFLight from './fonts/GmarketSansTTFLight.ttf';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
+const GlobalStyle = createGlobalStyle`
+@font-face {
+    font-family: 'GmarketSansTTFLight';
+    src: url(${GmarketSansTTFLight}) format('truetype');
+    font-weight: normal;
+    font-style: normal;
+}
+
+body {
+    font-family: 'GmarketSansTTFLight', sans-serif;
+    background: linear-gradient(to right, #89CFF0 0%, #B2FFFF 50%, #89CFF0 100%);
+}
+`;
+
 root.render(
-  <React.StrictMode>
+  <BrowserRouter>
+    <GlobalStyle />
     <App />
-  </React.StrictMode>
+  </BrowserRouter>
 );
 
 // If you want to start measuring performance in your app, pass a function
