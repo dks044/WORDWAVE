007d1b516138a63235520d1d3ab030cccc3bf341
ddolboghi <rhdfyd128@gmail.com>
2024-01-10 16:21:40 +0900

    test: Grammar 저장 테스트 코드 작성

--- src/test/java/com/wordwave/grammarbook/GrammarBookServiceTest.java
+++ src/test/java/com/wordwave/grammarbook/GrammarBookServiceTest.java
@@ -1,15 +1,18 @@
 package com.wordwave.grammarbook;
 
+import com.wordwave.grammar.GrammarDto;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.annotation.Rollback;
 import org.springframework.transaction.annotation.Transactional;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
 @SpringBootTest
 @Transactional
+@Rollback(false)
 //@TestPropertySource(locations = "classpath:application-test.properties")
 class GrammarBookServiceTest {
     @Autowired
@@ -18,11 +21,20 @@ class GrammarBookServiceTest {
     @Test
     @DisplayName("GrammarBook id로 GrammarBook을 조회한다.")
     void getGrammarBookTest() {
-        Long id = 4L;
+        Long id = 6L;
 
         GrammarBookDto grammarBookDto = grammarBookService.getGrammarBookById(id);
 
-        assertThat(grammarBookDto.getGrammarDtos().size()).isEqualTo(6);
         grammarBookDto.getGrammarDtos().forEach(grammarDto -> System.out.println(grammarDto.getSentence()));
+        assertThat(grammarBookDto.getGrammarDtos().size()).isEqualTo(1);
+    }
+
+    @Test
+    @DisplayName("GrammarBook에 Grammar를 저장한다.")
+    void saveGrammarTest() {
+        String grammarBookName = "test book3";
+        GrammarDto grammarDto1 = new GrammarDto("save grammar test3", grammarBookName);
+
+        this.grammarBookService.saveGrammar(grammarDto1);
     }
 }
