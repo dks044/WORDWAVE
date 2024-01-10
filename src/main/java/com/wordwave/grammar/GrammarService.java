50a42b17ebfe2d40c5ee2a1ee37bf4c500db9fa8
ddolboghi <rhdfyd128@gmail.com>
2024-01-10 16:27:30 +0900

    feat: Grammar 삭제 기능 추가

--- src/main/java/com/wordwave/grammar/GrammarService.java
+++ src/main/java/com/wordwave/grammar/GrammarService.java
@@ -1,8 +1,6 @@
 package com.wordwave.grammar;
 
 import com.wordwave.exception.DataNotFoundException;
-import com.wordwave.grammarbook.GrammarBook;
-import com.wordwave.grammarbook.GrammarBookRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
@@ -10,20 +8,14 @@ import org.springframework.stereotype.Service;
 @RequiredArgsConstructor
 public class GrammarService {
     private final GrammarRepository grammarRepository;
-    private final GrammarBookRepository grammarBookRepository;
-
-    public void createGrammar(GrammarDto grammarDto) {
-        GrammarBook grammarBook = grammarBookRepository.findByName(grammarDto.getGrammarBookName())
-                .orElseGet(GrammarBook.builder().name(grammarDto.getGrammarBookName())::build);
-        grammarRepository.save(Grammar.builder()
-                .sentence(grammarDto.getSentence())
-                .grammarBook(grammarBook)
-                .build());
-    }
 
     public GrammarDto getGrammar(Long id) {
         Grammar grammar = this.grammarRepository.findById(id)
                 .orElseThrow(() -> new DataNotFoundException("Grammar not found."));
         return new GrammarDto(grammar.getSentence(), grammar.getGrammarBook().getName());
+    }
+
+    public void deleteGrammar(Long id) {
+        this.grammarRepository.deleteById(id);
     }
 }
