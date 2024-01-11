666bd4fc0fc6c1b7ba89f54b47c4f9e9b4b5fba2
ddolboghi <rhdfyd128@gmail.com>
2024-01-11 15:43:05 +0900

    feat: GrammarBook 조회 API 구현

--- src/main/java/com/wordwave/grammarbook/GrammarBookController.java
+++ src/main/java/com/wordwave/grammarbook/GrammarBookController.java
@@ -1,15 +1,23 @@
 package com.wordwave.grammarbook;
 
 import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RequiredArgsConstructor
-@RequestMapping("/api/grammar")
+@RequestMapping("/api/grammarbook")
 @RestController
 public class GrammarBookController {
     private final GrammarBookService grammarBookService;
 
-
+    @GetMapping("/{id}")
+    public ResponseEntity<GrammarBookResponseDto> getGrammarBook(@PathVariable Long id) {
+        return ResponseEntity
+                .status(HttpStatus.OK)
+                .body(this.grammarBookService.getGrammarBook(id));
+    }
 }
