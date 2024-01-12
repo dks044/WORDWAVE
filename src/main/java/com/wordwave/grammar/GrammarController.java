921c544a0e2e937d451f4d7c9f4292003bf4b1f0
ddolboghi <rhdfyd128@gmail.com>
2024-01-12 16:15:30 +0900

    refactor: Grammar 조회 API 리턴 타입 수정

--- src/main/java/com/wordwave/grammar/GrammarController.java
+++ src/main/java/com/wordwave/grammar/GrammarController.java
@@ -3,10 +3,7 @@ package com.wordwave.grammar;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequiredArgsConstructor
@@ -15,7 +12,7 @@ public class GrammarController {
     private final GrammarService grammarService;
 
     @GetMapping("/{id}")
-    public ResponseEntity<GrammarDto> getGrammar(@PathVariable Long id) {
+    public ResponseEntity<GrammarResponseDto> getGrammar(@PathVariable Long id) {
         return ResponseEntity.status(HttpStatus.OK).body(this.grammarService.getGrammar(id));
     }
 }
