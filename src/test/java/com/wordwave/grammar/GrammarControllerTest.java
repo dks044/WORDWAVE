7c5a930175c0db43b6b21f2eed7508bc67b07c4d
ddolboghi <rhdfyd128@gmail.com>
2024-01-12 16:02:09 +0900

    test: Grammar 조회 API 기능 테스트 코드 작성

---
+++ src/test/java/com/wordwave/grammar/GrammarControllerTest.java
@@ -0,0 +1,30 @@
+package com.wordwave.grammar;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.http.HttpStatusCode;
+import org.springframework.http.ResponseEntity;
+import org.springframework.test.annotation.Rollback;
+import org.springframework.transaction.annotation.Transactional;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+@SpringBootTest
+@Transactional
+@Rollback(false)
+class GrammarControllerTest {
+    @Autowired
+    GrammarController grammarController;
+
+    @Test
+    @DisplayName("id로 grammar를 조회한다.")
+    void getGrammarApiTest() {
+        Long id = 16L;
+
+        ResponseEntity<GrammarDto> response = this.grammarController.getGrammar(id);
+
+        assertThat(response.getStatusCode()).isEqualTo(HttpStatusCode.valueOf(200));
+    }
+}
