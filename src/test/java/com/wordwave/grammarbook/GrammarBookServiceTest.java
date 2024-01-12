7fd47f7138d0ec2af3e1ca7f2a708b09f312420c
ddolboghi <rhdfyd128@gmail.com>
2024-01-12 17:37:59 +0900

    test: 모든 GrammarBook 조회 기능 테스트 코드 작성, GrammarBook 조회 기능 테스트 코드 수정

--- src/test/java/com/wordwave/grammarbook/GrammarBookServiceTest.java
+++ src/test/java/com/wordwave/grammarbook/GrammarBookServiceTest.java
@@ -8,6 +8,8 @@ import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.annotation.Rollback;
 import org.springframework.transaction.annotation.Transactional;
 
+import java.util.List;
+
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.junit.jupiter.api.Assertions.assertTrue;
 
@@ -29,7 +31,14 @@ class GrammarBookServiceTest {
         GrammarBookResponseDto grammarBookResponseDto = grammarBookService.getGrammarBook(id);
 
         System.out.println(grammarBookResponseDto.toString());
-        assertThat(grammarBookResponseDto.getGrammarDtos().size()).isEqualTo(4);
+    }
+
+    @Test
+    @DisplayName("모든 GrammarBook을 조회한다.")
+    void getAllGrammarBooksTest() {
+        List<GrammarBook> grammarBooks = this.grammarBookService.getAllGrammarBooks();
+
+        System.out.println(grammarBooks);
     }
 
     @Test
