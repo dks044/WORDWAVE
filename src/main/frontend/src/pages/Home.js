be629e1686147535f71570a52bd8f7864600bf3f
AJN <buzz781@gmail.com>
2024-01-13 17:20:33 +0900

    feat: Home 페이지 화면 구현 & NotFound.js 생성

--- src/main/frontend/src/pages/Home.js
+++ src/main/frontend/src/pages/Home.js
@@ -1,18 +1,25 @@
 import React from "react";
 import styled from "styled-components";
 
-
 const HomeContainer = styled.div`
     flex: 1;
-
+    justify-content: center;
+    text-align: center;
+    padding-top: 30%;
+    transition: 0.25s all ease-in-out;
 `;
-
+const Hr = styled.hr`
+    width: 30%;
+`
 
 
 const Home = () => {
     return (
         <HomeContainer>
-            
+            <h2>환영합니다!</h2>
+            <h1>WORDWAVE</h1>
+            <Hr></Hr>
+            <p>밑에 다이얼 메뉴 버튼을 눌러 시작하세요</p>
         </HomeContainer>
     )
 }
