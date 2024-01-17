8e6b9310d663b8e086821dec765aec518d3ee9ee
AJN <buzz781@gmail.com>
2024-01-18 01:13:21 +0900

    chore: 테스트코드 도메인 로직관련 리팩토링

---
+++ src/test/java/com/wordwave/user/UserTest.java
@@ -0,0 +1,11 @@
+package com.wordwave.user;
+
+import org.springframework.boot.test.context.SpringBootTest;
+
+import lombok.RequiredArgsConstructor;
+
+@SpringBootTest
+@RequiredArgsConstructor
+public class UserTest {
+
+}
--- src/test/java/com/wordwave/voca/VocaTest.java
+++ src/test/java/com/wordwave/voca/VocaTest.java
@@ -1,4 +1,4 @@
-package com.wordwave.main;
+package com.wordwave.voca;
 
 import org.junit.jupiter.api.Disabled;
 import org.junit.jupiter.api.Test;
