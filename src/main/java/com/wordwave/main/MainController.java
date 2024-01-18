361fb1d21ec4d07488c07018157daac891b6f2b8
ddolboghi <rhdfyd128@gmail.com>
2024-01-19 01:07:54 +0900

    feat: 리액트 빌드시 만들어지는 html로 라우트 매핑

--- src/main/java/com/wordwave/main/MainController.java
+++ src/main/java/com/wordwave/main/MainController.java
@@ -1,10 +1,12 @@
 package com.wordwave.main;
 
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
 
-@RestController
+@Controller
 public class MainController {
-	
-	
+	@RequestMapping({"/", "/grammar"})
+    public String redirect() {
+        return "index";
+    }
 }
