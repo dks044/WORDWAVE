07ec3902dd2fe39946d45f23f32710dca9880e71
ddolboghi <rhdfyd128@gmail.com>
2024-01-18 18:00:40 +0900

    feat: 문법 페이지 구현

--- src/main/frontend/src/components/DialMenu.js
+++ src/main/frontend/src/components/DialMenu.js
@@ -1,104 +1,130 @@
 import React, { useState } from "react";
 import styled, { css } from "styled-components";
 import { TiWaves } from "react-icons/ti";
+import { Link } from "react-router-dom";
+import grammarLogo from "../resources/grammar.svg";
 
 const CircleButton = styled.button`
-    //색 설정
-    background: skyblue;
-    color: white;
-    //사이즈 설정
-    width: 100px;
-    height: 100px;
-    border-radius: 50%;
-    //포지션 설정
-    position: absolute;
-    z-index: 5;
-    bottom: -30px;
-    left: 50%;
-    transform: translateX(-50%);
-    align-items: center;
-    justify-content: center;
-    //테두리 설정
-    border: none;
-    outline: none;
-    //이벤트 설정
-    cursor: pointer;
-    &:hover {
-        background: #63e6be;
-    }
-    &:active {
-        background: #20c997;
-    }
-    //애니메이션 설정
-    transition: 0.325s all ease-in;
-    //사용자정의 이벤트 설정
-    ${props =>
-        props.open &&
-        css`
-        background: #2E2EFD;
-        &:hover {
-            background: #5783FB;
-        }
-        &:active {
-            background: #0040FC;
-        }
-        transform: translateX(-50%) translateY(0%) rotate(180deg);
-        
+  //색 설정
+  background: skyblue;
+  color: white;
+  //사이즈 설정
+  width: 100px;
+  height: 100px;
+  border-radius: 50%;
+  //포지션 설정
+  position: absolute;
+  z-index: 5;
+  bottom: -30px;
+  left: 50%;
+  transform: translateX(-50%);
+  align-items: center;
+  justify-content: center;
+  //테두리 설정
+  border: none;
+  outline: none;
+  //이벤트 설정
+  cursor: pointer;
+  &:hover {
+    background: #63e6be;
+  }
+  &:active {
+    background: #20c997;
+  }
+  //애니메이션 설정
+  transition: 0.325s all ease-in;
+  //사용자정의 이벤트 설정
+  ${(props) =>
+    props.open &&
+    css`
+      background: #2e2efd;
+      &:hover {
+        background: #5783fb;
+      }
+      &:active {
+        background: #0040fc;
+      }
+      transform: translateX(-50%) translateY(0%) rotate(180deg);
     `}
-`
+`;
 const CircleMenu = styled.div`
-    //색 설정
-    background: skyblue;
-    color: white;
-    //사이즈 설정
-    width: 100px;
-    height: 100px;
-    border-radius: 50%;
-    //포지션 설정
-    position: absolute;
-    z-index: 4;
-    bottom: -30px;
-    left: 50%;
-    transform: translateX(-50%); 
-    //애니메이션 설정
-    transition: 0.25s all ease-in-out;
-    //초기 상태 설정
-    width: 0;
-    height: 0;
-    opacity: 0; // 투명도를 0으로 설정
-    bottom: -15px; // 0의 크기에서 시작할 때의 위치
+  //색 설정
+  background: skyblue;
+  color: white;
+  //사이즈 설정
+  width: 100px;
+  height: 100px;
+  border-radius: 50%;
+  //포지션 설정
+  position: absolute;
+  z-index: 4;
+  bottom: -30px;
+  left: 50%;
+  transform: translateX(-50%);
+  //애니메이션 설정
+  transition: 0.25s all ease-in-out;
+  //초기 상태 설정
+  width: 0;
+  height: 0;
+  opacity: 0; // 투명도를 0으로 설정
+  bottom: -15px; // 0의 크기에서 시작할 때의 위치
 
-    ${props =>
-        props.open &&
-        css`
-            width: 300px;
-            height: 300px;
-            bottom: -122px;
-            opacity: 1; // open일 때 투명도를 1로 설정
-        `
-    }
+  ${(props) =>
+    props.open &&
+    css`
+      width: 300px;
+      height: 300px;
+      bottom: -122px;
+      opacity: 1; // open일 때 투명도를 1로 설정
+    `}
 `;
 
+const GrammarLink = styled(Link)`
+  //색 설정
+  color: black;
+  text-decoration: none;
+  //사이즈 설정
+  width: 100px;
+  height: 100px;
+  //내부 요소들의 포지션 설정
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+  //다이얼 메뉴내 GrammaarLink의 위치
+  position: absolute;
+  left: 50%;
+  top: 5%;
+  transform: translateX(-50%);
+`;
 
+const GrammarLogo = styled.img`
+  width: 30%;
+  height: 30%;
+`;
 
-function DialMenu(){
-    const [open,setOpen] = useState(false);
-    const onToggle = () =>{
-        setOpen(!open);
-        console.log(open);
-    };
-    
-    return (
-        <>
-            <CircleMenu open={open}>
-            
-            </CircleMenu>
-            <CircleButton onClick={onToggle} open={open}>
-                <TiWaves size={85}/>
-            </CircleButton>
-        </>
-    );
+function DialMenu() {
+  const [open, setOpen] = useState(false);
+  const onToggle = () => {
+    setOpen(!open);
+    console.log(open);
+  };
 
+  return (
+    <>
+      <CircleMenu open={open}>
+        {open && (
+          <GrammarLink to="/grammar">
+            <GrammarLogo src={grammarLogo} alt="grammar logo" />
+            문법
+          </GrammarLink>
+        )}
+      </CircleMenu>
+      <CircleButton onClick={onToggle} open={open}>
+        <TiWaves size={85} />
+      </CircleButton>
+    </>
+  );
 }
 
 export default React.memo(DialMenu);
--- src/main/frontend/src/pages/GrammarPage.js
+++ src/main/frontend/src/pages/GrammarPage.js
@@ -1,5 +1,32 @@
+import React, { useState } from "react";
+import styled from "styled-components";
+
+const StartButton = styled.button`
+  //색 설정
+  background: #2e2efd;
+  //사이즈 설정
+  width: 50%;
+  height: 50px;
+  //포지션 설정
+  position: absolute;
+  top: 40%;
+  transform: translateX(50%);
+  //테두리 설정
+  border-style: none;
+  border-radius: 12px;
+  //글자 설정
+  text-align: center;
+  color: white;
+`;
+
 function GrammarPage() {
-  return "grammarpage test";
+  //TODO: 버튼 누르면 grammar book 종류 고르고 해당 grammar book 서버에서 가져오기
+
+  return (
+    <>
+      <StartButton>문법 퀴즈 시작</StartButton>
+    </>
+  );
 }
 
 export default GrammarPage;
