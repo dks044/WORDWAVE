19a5387ec7ceee14ff1fb54a3dc9d19b39e87fd7
ddolboghi <rhdfyd128@gmail.com>
2024-01-19 17:37:27 +0900

    fix: csrf 설정 수정

--- src/main/java/com/wordwave/security/SecurityConfig.java
+++ src/main/java/com/wordwave/security/SecurityConfig.java
@@ -5,7 +5,7 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
-import org.springframework.security.config.annotation.web.configuration.WebSecurityConfiguration;
+import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.web.header.writers.frameoptions.XFrameOptionsHeaderWriter;
 import org.springframework.security.web.util.matcher.AntPathRequestMatcher;
@@ -16,7 +16,7 @@ public class SecurityConfig {
     @Bean
     SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
         http
-	        .csrf().disable()
+	        .csrf(AbstractHttpConfigurer::disable)
 	        .authorizeHttpRequests((authorizeHttpRequests) -> authorizeHttpRequests
 	            .requestMatchers(new AntPathRequestMatcher("/**")).permitAll())
 	        .headers((headers) -> headers
