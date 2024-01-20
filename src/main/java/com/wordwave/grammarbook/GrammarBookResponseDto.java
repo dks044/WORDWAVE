6d3d1172ae8b6834980acff3939998f4e92d499c
ddolboghi <rhdfyd128@gmail.com>
2024-01-20 19:49:21 +0900

    refactor: GrammarBookResponseDto에 생성자 추가

--- src/main/java/com/wordwave/grammarbook/GrammarBookResponseDto.java
+++ src/main/java/com/wordwave/grammarbook/GrammarBookResponseDto.java
@@ -1,11 +1,15 @@
 package com.wordwave.grammarbook;
 
 import com.wordwave.grammar.GrammarResponseDto;
+import lombok.AllArgsConstructor;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 
 import java.util.List;
 
 @Data
+@AllArgsConstructor
+@NoArgsConstructor
 public class GrammarBookResponseDto {
 
     private Long id;
