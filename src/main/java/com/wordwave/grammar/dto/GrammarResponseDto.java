df59ebf6c98c3a70f78e75451cb47fdca3d15a5a
ddolboghi <rhdfyd128@gmail.com>
2024-01-24 17:39:13 +0900

    refactor: Grammar 조회 & 문장 수정 기능 수정, 보기 저장 기능 추가

--- src/main/java/com/wordwave/grammar/dto/GrammarResponseDto.java
+++ src/main/java/com/wordwave/grammar/dto/GrammarResponseDto.java
@@ -1,6 +1,8 @@
-package com.wordwave.grammar;
+package com.wordwave.grammar.dto;
 
 import lombok.*;
+
+import java.util.List;
 
 @Getter
 @Setter
@@ -12,6 +14,8 @@ public class GrammarResponseDto {
     private Long id;
 
     private String sentence;
+
+    private List<GrammarExampleDto> grammarExampleDtos;
 
     private String grammarBookName;
 }
