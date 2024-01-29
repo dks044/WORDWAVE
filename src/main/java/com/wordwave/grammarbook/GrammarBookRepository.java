f26338c4ebe7f84e4be2d66b660d94104f74a9b4
ddolboghi <rhdfyd128@gmail.com>
2024-01-30 02:18:50 +0900

    feat: GrammarBook 조회 JPQL 작성 refactor: jakarta.annotation.Nonnull 추가

--- src/main/java/com/wordwave/grammarbook/GrammarBookRepository.java
+++ src/main/java/com/wordwave/grammarbook/GrammarBookRepository.java
@@ -1,5 +1,7 @@
+
 package com.wordwave.grammarbook;
 
+import jakarta.annotation.Nonnull;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
@@ -11,10 +13,13 @@ import java.util.Optional;
 public interface GrammarBookRepository extends JpaRepository<GrammarBook, Long> {
 
     @Override
+    @Nonnull
     @Query("select gb from GrammarBook gb join fetch gb.grammars where gb.id = :id")
-    Optional<GrammarBook> findById(@Param("id") Long id);
+    Optional<GrammarBook> findById(@Nonnull @Param("id") Long id);
 
     @Query("select gb from GrammarBook gb join fetch gb.grammars where gb.name = :name")
     Optional<GrammarBook> findByName(@Param("name") String name);
 
+    @Query("select distinct gb from GrammarBook gb left join fetch gb.grammars g left join fetch g.examples ex where gb.id = :id")
+    Optional<GrammarBook> findGrammarBookById(@Param("id") Long id);
 }
