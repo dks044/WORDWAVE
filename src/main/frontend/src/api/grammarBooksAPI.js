52426285ea0f595771a796ac4960fe8d69c3f0fb
ddolboghi <rhdfyd128@gmail.com>
2024-02-02 17:18:54 +0900

    fix: axios GET 요청 오류 해결

--- src/main/frontend/src/api/grammarBooksAPI.js
+++ src/main/frontend/src/api/grammarBooksAPI.js
@@ -1,15 +1,11 @@
 import axios from "axios";
 
-const instance = axios.create({
-  baseURL: "http://localhost:8080/",
-});
-
 export const getGrammarBooks = async () => {
-  const response = await instance.get("api/grammarbook/all");
+  const response = await axios.get("/api/grammarbook/all");
   return response.data;
 };
 
 export const getGrammarBookById = async (id) => {
-  const response = await instance.get(`api/grammarbook/${id}`);
+  const response = await axios.get(`/api/grammarbook/${id}`);
   return response.data;
 };
