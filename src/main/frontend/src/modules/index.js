c9710f5d80e87d7b7cbd18ac2002c0850f7d9c8d
ddolboghi <rhdfyd128@gmail.com>
2024-02-02 17:17:06 +0900

    refactor: redux 설정 수정

--- src/main/frontend/src/modules/index.js
+++
@@ -1,7 +0,0 @@
-import { combineReducers } from 'redux';
-import auth from '../modules/auth';
-import popup from '../modules/popup';
-
-const rootReducer = combineReducers({auth,popup});
-
-export default rootReducer;
