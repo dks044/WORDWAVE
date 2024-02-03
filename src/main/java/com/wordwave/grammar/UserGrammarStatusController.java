3f66050b768bb1a35ea334f34373ab767ed32904
ddolboghi <rhdfyd128@gmail.com>
2024-02-03 15:10:07 +0900

    feat: 사용자의 문법 오답 테이블 CRUD API 추가

--- src/main/java/com/wordwave/grammar/UserGrammarStatusController.java
+++ src/main/java/com/wordwave/grammar/UserGrammarStatusController.java
@@ -1,13 +1,12 @@
 package com.wordwave.grammar;
 
-import com.wordwave.grammar.dto.WrongGrammarDto;
+import com.wordwave.grammar.dto.WrongGrammarsDto;
 import lombok.RequiredArgsConstructor;
-import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.security.access.prepost.PreAuthorize;
+import org.springframework.web.bind.annotation.*;
+
+import java.security.Principal;
 
 @RestController
 @RequiredArgsConstructor
@@ -15,9 +14,49 @@ import org.springframework.web.bind.annotation.RestController;
 public class UserGrammarStatusController {
     private final UserGrammarStatusService userGrammarStatusService;
 
-    @PostMapping("/")
-    public ResponseEntity<?> saveUserWrongGrammars(@RequestBody WrongGrammarDto wrongGrammarDto) {
-        this.userGrammarStatusService.saveUserWrongGrammars(wrongGrammarDto);
-        return ResponseEntity.status(HttpStatus.OK).body("Saved wrong grammars.");
+    @GetMapping()
+    public ResponseEntity<?> getUserWrongGrammars(Principal principal) {
+        String userName = principal.getName();
+        if (userName == null || userName.isBlank()) {
+            return ResponseEntity.status(403).body("Forbidden error: Unauthenticated user request");
+        }
+        try {
+            return ResponseEntity.ok().body(this.userGrammarStatusService.getUserWrongGrammars(userName));
+        } catch (Exception e) {
+            return ResponseEntity.status(404).body(e.getMessage());
+        }
+    }
+
+    @PreAuthorize("isAuthenticated()")
+    @PostMapping()
+    public ResponseEntity<?> saveUserWrongGrammars(@RequestBody WrongGrammarsDto wrongGrammarsDto) {
+        try {
+            this.userGrammarStatusService.saveUserWrongGrammars(wrongGrammarsDto);
+            return ResponseEntity.ok().body("Saved wrong grammars.");
+        } catch (Exception e) {
+            return ResponseEntity.status(404).body(e.getMessage());
+        }
+    }
+
+    @PreAuthorize("isAuthenticated()")
+    @PutMapping()
+    public ResponseEntity<?> updateUserLastTryTime(@RequestBody WrongGrammarsDto wrongGrammarsDto) {
+        try {
+            this.userGrammarStatusService.updateUserLastTryTime(wrongGrammarsDto);
+            return ResponseEntity.ok().body("Updated last try time.");
+        } catch (Exception e) {
+            return ResponseEntity.status(404).body(e.getMessage());
+        }
+    }
+
+    @PreAuthorize("isAuthenticated()")
+    @DeleteMapping()
+    public ResponseEntity<?> deleteUserWrongGrammars(@RequestBody WrongGrammarsDto wrongGrammarsDto) {
+        try {
+            this.userGrammarStatusService.deleteUserWrongGrammars(wrongGrammarsDto);
+            return ResponseEntity.ok().body("Deleted wrong grammars.");
+        } catch (Exception e) {
+            return ResponseEntity.status(404).body(e.getMessage());
+        }
     }
 }
