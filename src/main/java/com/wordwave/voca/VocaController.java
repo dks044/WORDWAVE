ae2b77988dd7060eef484c710f3089d515b086a0
AJN <buzz781@gmail.com>
2024-02-04 16:21:12 +0900

    feat: vocaBookList 가져오는 컨트롤 메소드

--- src/main/java/com/wordwave/voca/VocaController.java
+++ src/main/java/com/wordwave/voca/VocaController.java
@@ -1,9 +1,14 @@
 package com.wordwave.voca;
 
+import java.util.List;
+
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.wordwave.vocabook.VocaBook;
+import com.wordwave.vocabook.VocaBookService;
 import lombok.RequiredArgsConstructor;
 
 @RequiredArgsConstructor
@@ -11,6 +16,17 @@ import lombok.RequiredArgsConstructor;
 @RequestMapping("/api/voca")
 public class VocaController {
 	
+	VocaBookService vocaBookService;
 	
+	@GetMapping("/vocabooklist")
+	public ResponseEntity<?> selectVocaBook(){
+		try {
+			List<VocaBook> vocaBookList = vocaBookService.getVocaBookList();
+			return ResponseEntity.ok().body(vocaBookList);
+		} catch (Exception e) {
+			e.printStackTrace();
+			return ResponseEntity.badRequest().body("vocabook import falid");
+		}
+	}
 	
 }
