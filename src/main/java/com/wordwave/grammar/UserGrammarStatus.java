91b06765f2cc72b176e5de35a3893b7084214385
ddolboghi <rhdfyd128@gmail.com>
2024-02-05 20:10:02 +0900

    feat: UserGrammarStatus 엔티티에 grammarBookId 필드 추가

--- src/main/java/com/wordwave/grammar/UserGrammarStatus.java
+++ src/main/java/com/wordwave/grammar/UserGrammarStatus.java
@@ -22,15 +22,18 @@ public class UserGrammarStatus {
     private SiteUser user;
 
     /**
-     * Grammar 엔티티에서 UserGrammarStatus의 PK가 아닌 FK인 user를 참조해야하므로 ORM 설계 관점에서 좋지 않다.
+     * Grammar 엔티티에서 UserGrammarStatus의 PK가 아닌 FK인 user를 참조해야하므로 ORM 설계 관점에서 좋지 않습니다.
      * 게다가 Grammar에서는 레코드당 여러 user를 매핑해야하는데,
-     * 이는 UserGrammarStatus에서 user 한 명당 여러 Grammar를 매핑해야하는 상황과 정반대다.
-     * 이는 N:N 관계로 풀 수 있겠지만 쓸데없이 복잡해진다는 단점이 있다.
-     * <p>그래서 연관관계를 설정해주는 대신 클라이언트로부터 grammar_id를 받아와 저장하고 필요할때 Grammar에 join한다.
-     * <p>user 한 명당 grammar_id는 여러 개가 저장될 수 있다.
+     * 반대로 UserGrammarStatus에서는 user 한 명당 여러 Grammar를 매핑해야 합니다.
+     * 이는 N:N 관계로 풀 수 있지만 쓸데없이 복잡해집니다.
+     * <p>그래서 연관관계를 설정해주는 대신 클라이언트로부터 grammarId를 받아와 저장하고 필요할때 Grammar에 join합니다.
+     * <p>user 한 명당 grammarId는 여러 개가 저장될 수 있지만, 한 명의 user가 중복되는 grammarId를 가질 수 없습니다.
+     * <p>grammarBookId도 같은 개념을 적용하며, grammarId를 저장할때 grammar로부터 불러와 grammarBookId를 저장합니다.
      */
     @Column(unique = true)
     private Long wrongGrammarId;
+
+    private Long grammarBookId;
 
     @DateTimeFormat(pattern = "yyyy-MM-ddTHH:mm:ss")
     private LocalDateTime lastTryTime;
@@ -40,9 +43,10 @@ public class UserGrammarStatus {
     }
 
     @Builder
-    public UserGrammarStatus(SiteUser user, Long wrongGrammarId, LocalDateTime lastTryTime) {
+    public UserGrammarStatus(SiteUser user, Long wrongGrammarId, Long grammarBookId, LocalDateTime lastTryTime) {
         this.user = user;
         this.wrongGrammarId = wrongGrammarId;
+        this.grammarBookId = grammarBookId;
         this.lastTryTime = lastTryTime;
     }
 }
