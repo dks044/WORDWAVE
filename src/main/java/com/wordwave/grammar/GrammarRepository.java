078f1a25cdf0b3fbc996aafcecef56277a9a7938
ddolboghi <rhdfyd128@gmail.com>
2024-02-05 20:08:23 +0900

    feat: grammarId로 grammarBookId를 가져오는 JPQL 추가

--- src/main/java/com/wordwave/grammar/GrammarRepository.java
+++ src/main/java/com/wordwave/grammar/GrammarRepository.java
@@ -1,9 +1,12 @@
 package com.wordwave.grammar;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
 @Repository
 public interface GrammarRepository extends JpaRepository<Grammar, Long> {
-
+    @Query("select g.grammarBook.id from Grammar g where g.id = :id")
+    Long findGrammarBookIdByGrammarId(@Param("id") Long id);
 }
