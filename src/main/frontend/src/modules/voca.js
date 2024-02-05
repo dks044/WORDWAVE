911c9aacf37581b6cae1e87c0e5e06718b41c2d7
AJN <buzz781@gmail.com>
2024-02-05 14:46:14 +0900

    feat: 리듀서 코드 작성

--- src/main/frontend/src/modules/voca.js
+++ src/main/frontend/src/modules/voca.js
@@ -9,9 +9,14 @@ const GET_VOCABOOKLIST = 'GET_VOCABOOKLIST';
 const GET_VOCABOOKLIST_SUCCESS = 'GET_VOCABOOKLIST_SUCCESS';
 const GET_VOCABOOKLIST_FAILURE = 'GET_VOCABOOKLIST_FAILURE';
 
+const GET_VOCABOOK_DETAIL = 'GET_VOCABOOK_DETAIL';
+const GET_VOCABOOK_DETAIL_SUCCESS = 'GET_VOCABOOK_DETAIL_SUCCESS';
+const GET_VOCABOOK_DETAIL_FAILURE = 'GET_VOCABOOK_DETAIL_FAILURE';
+
+
 const initialState = {
   vocaBooks : reducerUtils.initial(),
-  vocaBook : {}
+  vocaBook : reducerUtils.initial()
 }
 
 
@@ -27,12 +32,27 @@ export const getVocaBookList = () => async dispatch => {
   }
 }
 
+export const getVocaBookDetail = (vocaBookId) => async dispatch => {
+  dispatch({type : GET_VOCABOOK_DETAIL});
+  try {
+    const response = await vocaAPI.getVocaBookDetail(vocaBookId);
+    dispatch({type : GET_VOCABOOK_DETAIL_SUCCESS, payload : response.data})
+  } catch (e) {
+    dispatch({type : GET_VOCABOOK_DETAIL_FAILURE,error : e});
+    throw e;
+  }
+}
+
 export default function voca(state = initialState, action) {
   switch (action.type) {
     case GET_VOCABOOKLIST:
     case GET_VOCABOOKLIST_SUCCESS:
     case GET_VOCABOOKLIST_FAILURE:
       return handleAsyncActions(GET_VOCABOOKLIST, 'vocaBooks')(state, action);
+    case GET_VOCABOOK_DETAIL:
+    case GET_VOCABOOK_DETAIL_SUCCESS:
+    case GET_VOCABOOK_DETAIL_FAILURE:
+      return handleAsyncActionsById(GET_VOCABOOK_DETAIL,'vocaBook')(state,action);
     default:
       return state;
   }
