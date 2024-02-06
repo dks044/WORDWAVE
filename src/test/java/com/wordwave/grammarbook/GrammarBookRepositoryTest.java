35b3c01a80e647788fba439526e4c5ce7b2f3960
ddolboghi <rhdfyd128@gmail.com>
2024-02-06 22:15:50 +0900

    test: GrammarBook의 id로 포함된 Grammar들만 조회하는 테스트 코드 작성

--- src/test/java/com/wordwave/grammarbook/GrammarBookRepositoryTest.java
+++ src/test/java/com/wordwave/grammarbook/GrammarBookRepositoryTest.java
@@ -1,13 +1,12 @@
 package com.wordwave.grammarbook;
 
-import com.wordwave.grammar.Grammar;
-import org.assertj.core.util.Lists;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.test.annotation.Rollback;
 import org.springframework.transaction.annotation.Transactional;
+
+import static org.assertj.core.api.Assertions.assertThat;
 
 @SpringBootTest
 @Transactional
@@ -16,22 +15,19 @@ class GrammarBookRepositoryTest {
     GrammarBookRepository grammarBookRepository;
 
     @Test
-//    @Rollback(false)
-    void cascadePersistTest() {
-        GrammarBook grammarBook = GrammarBook.builder().name("test book2").build();
-        Grammar grammar1 = Grammar.builder().sentence("test sentence4").grammarBook(grammarBook).build();
-        Grammar grammar2 = Grammar.builder().sentence("test sentence5").grammarBook(grammarBook).build();
-        Grammar grammar3 = Grammar.builder().sentence("test sentence6").grammarBook(grammarBook).build();
-
-        grammarBook.getGrammars().addAll(Lists.newArrayList(grammar1, grammar2, grammar3));
-
-        grammarBookRepository.save(grammarBook);
-    }
-
-    @Test
     @DisplayName("GrammarBook을 전체 조회한다.")
     void findAllTest() {
         System.out.println(this.grammarBookRepository.findAll());
     }
 
+    @Test
+    @DisplayName("GrammarBook에서 Grammar를 참조할때 null값이 포함되지 않는다.")
+    void getGrammarsTest() {
+        Long grammarBookId = 5L;
+
+        GrammarBook grammarBook = this.grammarBookRepository.findById(grammarBookId).get();
+
+        assertThat(grammarBook.getGrammars().size()).isEqualTo(10);
+        System.out.println(grammarBook.getGrammars());
+    }
 }
