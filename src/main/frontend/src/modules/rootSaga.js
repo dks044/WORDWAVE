b02e02dd2205fac388f24ad1c9b7373745fa1e69
AJN <buzz781@gmail.com>
2024-02-07 03:39:19 +0900

    chore&feat : rootSaga.js 작성

---
+++ src/main/frontend/src/modules/rootSaga.js
@@ -0,0 +1,11 @@
+import createSagaMiddleware from "redux-saga";
+import { all, call } from "redux-saga/effects";
+
+
+const sagaMiddleware = createSagaMiddleware();
+
+export function* rootSaga() {
+  yield all([]);
+}
+
+export default sagaMiddleware;
