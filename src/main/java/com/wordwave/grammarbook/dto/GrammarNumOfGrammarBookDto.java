f66d60e07c899a582d8b8467decbf38117e6c575
ddolboghi <rhdfyd128@gmail.com>
2024-02-06 22:18:37 +0900

    feat: 모든 GrammarBook이 각각 가진 Grammar 개수 조회 기능 추가

---
+++ src/main/java/com/wordwave/grammarbook/dto/GrammarNumOfGrammarBookDto.java
@@ -0,0 +1,18 @@
+package com.wordwave.grammarbook.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
+public class GrammarNumOfGrammarBookDto {
+    private Long id;
+
+    private String name;
+
+    private Integer grammarNum;
+}
