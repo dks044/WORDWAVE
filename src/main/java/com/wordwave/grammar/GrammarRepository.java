2e48524790a4a5db998a2e736219add67ef7acba
ddolboghi <rhdfyd128@gmail.com>
2024-02-06 22:18:37 +0900

    feat: 모든 GrammarBook이 각각 가진 Grammar 개수 조회 기능 추가

--- src/main/java/com/wordwave/grammar/GrammarRepository.java
+++ src/main/java/com/wordwave/grammar/GrammarRepository.java
@@ -5,8 +5,12 @@ import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
+
 @Repository
 public interface GrammarRepository extends JpaRepository<Grammar, Long> {
     @Query("select g.grammarBook.id from Grammar g where g.id = :id")
     Long findGrammarBookIdByGrammarId(@Param("id") Long id);
+
+    List<Grammar> findByGrammarBookId(Long id);
 }
