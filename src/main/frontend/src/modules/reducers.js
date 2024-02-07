3c4af6c27d6d1e3a6595329b505bd9db0bc1dae0
AJN <buzz781@gmail.com>
2024-02-07 16:00:02 +0900

    fix: vocaBook rootRecuder에 연결

--- src/main/frontend/src/modules/reducers.js
+++ src/main/frontend/src/modules/reducers.js
@@ -1,8 +1,10 @@
 import { combineReducers } from "@reduxjs/toolkit";
 import auth from "./auth";
 import popup from "./popup";
+import vocaBook from "./voca/vocaBook";
 
 const rootReducer = combineReducers({
+  vocaBook : vocaBook,
   auth,
   popup,
 });
