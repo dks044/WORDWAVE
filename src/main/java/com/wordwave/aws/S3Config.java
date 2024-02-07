c93517580267bc712339bc88bc5a931408722c58
AJN <buzz781@gmail.com>
2024-02-07 20:07:10 +0900

    fix: aws 설정 코드 리팩토링

--- src/main/java/com/wordwave/aws/S3Config.java
+++ src/main/java/com/wordwave/aws/S3Config.java
@@ -6,6 +6,7 @@ import org.springframework.context.annotation.Configuration;
 
 import com.amazonaws.auth.AWSStaticCredentialsProvider;
 import com.amazonaws.auth.BasicAWSCredentials;
+import com.amazonaws.services.s3.AmazonS3;
 import com.amazonaws.services.s3.AmazonS3Client;
 import com.amazonaws.services.s3.AmazonS3ClientBuilder;
 
@@ -21,8 +22,8 @@ public class S3Config {
     @Value("${cloud.aws.region.static}")
     private String region;
 
-    @Bean
-    public AmazonS3Client amazonS3Client() {
+     @Bean
+     AmazonS3 amazonS3() {
         BasicAWSCredentials credentials = new BasicAWSCredentials(accessKey, secretKey);
 
         return (AmazonS3Client) AmazonS3ClientBuilder
