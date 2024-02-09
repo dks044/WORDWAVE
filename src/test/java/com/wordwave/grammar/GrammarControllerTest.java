521df9c2870762d2c5f7c67492b343e18ed4c6f1
ddolboghi <rhdfyd128@gmail.com>
2024-02-09 23:24:20 +0900

    refactor: Grammar 조회 API 테스트 코드 수정

--- src/test/java/com/wordwave/grammar/GrammarControllerTest.java
+++ src/test/java/com/wordwave/grammar/GrammarControllerTest.java
@@ -28,7 +28,7 @@ class GrammarControllerTest {
     void getGrammarApiTest() {
         Long id = 16L;
 
-        ResponseEntity<GrammarDto> response = this.grammarController.getGrammar(id);
+        ResponseEntity<Object> response = this.grammarController.getGrammar(id);
 
         assertThat(response.getStatusCode()).isEqualTo(HttpStatusCode.valueOf(200));
     }
