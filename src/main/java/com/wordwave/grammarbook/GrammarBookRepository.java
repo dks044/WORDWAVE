399b5b43ebd4412d0eefdeb8870490ac81210e4e
ddolboghi <rhdfyd128@gmail.com>
2024-02-09 23:15:17 +0900

    refactor: GrammarBookRepository의 findByName 파라미터 Nonnull조건 추가

--- src/main/java/com/wordwave/grammarbook/GrammarBookRepository.java
+++ src/main/java/com/wordwave/grammarbook/GrammarBookRepository.java
@@ -18,7 +18,7 @@ public interface GrammarBookRepository extends JpaRepository<GrammarBook, Long> 
     Optional<GrammarBook> findById(@Nonnull @Param("id") Long id);
 
     @Query("select gb from GrammarBook gb join fetch gb.grammars where gb.name = :name")
-    Optional<GrammarBook> findByName(@Param("name") String name);
+    Optional<GrammarBook> findByName(@Nonnull @Param("name") String name);
 
     @Query("select gb.id from GrammarBook gb where gb.name = :name")
     Long findIdByName(@Param("name") String name);
