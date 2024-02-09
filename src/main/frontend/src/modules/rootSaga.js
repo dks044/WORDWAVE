fd16a573fd493b0a6107216ccb6f0a316bbba498
ddolboghi <rhdfyd128@gmail.com>
2024-02-09 23:29:09 +0900

    feat: rootSaga에 grammarsSaga 추가

--- src/main/frontend/src/modules/rootSaga.js
+++ src/main/frontend/src/modules/rootSaga.js
@@ -2,11 +2,16 @@ import createSagaMiddleware from "redux-saga";
 import { all, call } from "redux-saga/effects";
 import grammarBooksSaga from "./grammarbooks/grammarBooksSaga";
 import wrongGrammarsSaga from "./wrong-grammars/wrongGrammarsSaga";
+import grammarsSaga from "./grammars/grammarsSaga";
 
 const sagaMiddleware = createSagaMiddleware();
 
 export function* rootSaga() {
-  yield all([call(grammarBooksSaga), call(wrongGrammarsSaga)]);
+  yield all([
+    call(grammarBooksSaga),
+    call(wrongGrammarsSaga),
+    call(grammarsSaga),
+  ]);
 }
 
 export default sagaMiddleware;
