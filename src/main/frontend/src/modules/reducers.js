3abe774bf913fb1b8ff63c0e1dc3788938920682
ddolboghi <rhdfyd128@gmail.com>
2024-02-09 23:28:39 +0900

    feat: rootReducer에 grammars 리듀서 추가

--- src/main/frontend/src/modules/reducers.js
+++ src/main/frontend/src/modules/reducers.js
@@ -1,5 +1,6 @@
 import { combineReducers } from "@reduxjs/toolkit";
 import grammarBooksReducer from "./grammarbooks/grammarBooks";
+import grammars from "./grammars/grammars";
 import grammarQuizReducer from "./quiz/grammarQuiz";
 import wrongGrammars from "./wrong-grammars/wrongGrammars";
 import auth from "./auth";
@@ -7,6 +8,7 @@ import popup from "./popup";
 
 const rootReducer = combineReducers({
   grammarBooks: grammarBooksReducer,
+  grammars: grammars,
   grammarQuiz: grammarQuizReducer,
   wrongGrammars: wrongGrammars,
   auth,
