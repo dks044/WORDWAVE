29e7a87ac2f099b93ac4345d76b81e3fd2199567
ddolboghi <rhdfyd128@gmail.com>
2024-02-15 17:24:51 +0900

    feat: 한번에 여러 개의 UserWrongGrammar 삭제하는 JPQL 추가

--- src/main/java/com/wordwave/grammar/UserWrongGrammarRepository.java
+++ src/main/java/com/wordwave/grammar/UserWrongGrammarRepository.java
@@ -1,6 +1,9 @@
 package com.wordwave.grammar;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Modifying;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
 import java.util.List;
@@ -12,4 +15,8 @@ public interface UserWrongGrammarRepository extends JpaRepository<UserWrongGramm
     List<UserWrongGrammar> findByUserId(Long userId);
     List<UserWrongGrammar> findByUserIdAndGrammarBookId(Long userId, Long grammarBookId);
     Optional<UserWrongGrammar> findByUserIdAndWrongGrammarId(Long userId, Long wrongGrammarId);
+
+    @Modifying
+    @Query("delete from UserWrongGrammar uwg where uwg.id in :userWrongGrammarIds")
+    void deleteByIds(@Param("userWrongGrammarIds") List<Long> userWrongGrammarIds);
 }
