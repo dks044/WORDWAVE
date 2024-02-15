a963cf21e53a981a50eff918e0e02d212c4d3604
ddolboghi <rhdfyd128@gmail.com>
2024-02-15 15:20:44 +0900

    remove: GrammarResultPage 삭제

--- src/main/frontend/src/pages/grammar/GrammarResultPage.js
+++
@@ -1,7 +0,0 @@
-import GrammarResultContainer from "../../containers/grammar/GrammarResultContainer";
-
-const GrammarResultPage = () => {
-  return <GrammarResultContainer />;
-};
-
-export default GrammarResultPage;
