357152906da11737bee2e5eb991bf73010661ee7
ddolboghi <rhdfyd128@gmail.com>
2024-02-15 17:59:44 +0900

    feat: 오답 문제 체크 버튼 구현

--- src/main/frontend/src/components/grammar/GrammarBooks.js
+++ src/main/frontend/src/components/grammar/GrammarBooks.js
@@ -1,5 +1,5 @@
 import { Link } from "react-router-dom";
-import { Button } from "react-bootstrap";
+import { Button, Form } from "react-bootstrap";
 import styled from "styled-components";
 
 const GrammarBookLink = styled(Link)`
@@ -43,15 +43,55 @@ const Lists = styled.div`
   transform: translateX(-50%);
 `;
 
-const GrammarBooks = ({ grammarBooks }) => {
+const CheckCustom = styled(Form.Check)`
+  //크기
+  width: 50px;
+  //위치
+  position: relative;
+  left: 110%;
+`;
+
+const Bubble = styled.div`
+  //스타일
+  background: #484848;
+  color: white;
+  border-radius: 5px;
+  padding: 12px 12.8px;
+  //크기
+  width: 205px;
+  height: 60px;
+  //위치
+  position: absolute;
+  top: 12%;
+  left: 40%;
+
+  &:after {
+    border-top: 10px solid #484848;
+    border-left: 10px solid transparent;
+    border-right: 10px solid transparent;
+    border-bottom: 0px solid transparent;
+    content: "";
+    position: absolute;
+    top: 60px;
+    left: 160px;
+  }
+`;
+
+const GrammarBooks = ({ grammarBooks, isLoging }) => {
   return (
     <>
+      <Bubble>버튼을 클릭하면 틀린 문제만 풀 수 있어요!</Bubble>
       <Lists>
         {grammarBooks.map((grammarBook) => (
           <ListItem key={grammarBook.id} size="lg">
             <GrammarBookLink to={`/grammarbooks/${grammarBook.name}`}>
               {grammarBook.name}
             </GrammarBookLink>
+            <CheckCustom
+              type="switch"
+              id="custom-switch"
+              feedbackTooltip="true"
+            />
           </ListItem>
         ))}
       </Lists>
