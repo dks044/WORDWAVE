5b3aefd8891ccb455d143d6d22d3f6d126b6bfc0
AJN <buzz781@gmail.com>
2024-02-17 15:14:54 +0900

    feat: vocaAPI 구현

--- src/main/java/com/wordwave/voca/VocaController.java
+++ src/main/java/com/wordwave/voca/VocaController.java
@@ -4,6 +4,7 @@ import java.util.List;
 
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -21,6 +22,17 @@ import lombok.RequiredArgsConstructor;
 @RequestMapping("/api/voca")
 public class VocaController {
 	
-
+	private final VocaService vocaService;
+	
+	@GetMapping("/{vocaBookId}/{category}")
+	public ResponseEntity<?> select(@PathVariable("vocaBookId") long vocaBookId,
+									@PathVariable("category") String category
+			){
+		try {
+			return ResponseEntity.ok().body(vocaService.select(vocaBookId, category));
+		} catch (Exception e) {
+			return ResponseEntity.badRequest().body("voca 불러오기 실패!");
+		}
+	}
 	
 }
