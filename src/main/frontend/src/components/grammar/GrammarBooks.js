98e0f34490a5d8bf47ffc4a76f6c54f4e7e2870c
ddolboghi <rhdfyd128@gmail.com>
2024-02-21 01:06:43 +0900

    feat & design: <WriteSwitch />(주관식 버튼) 추가 & 말풍선 삭제 및 디자인 수정

--- src/main/frontend/src/components/grammar/GrammarBooks.js
+++ src/main/frontend/src/components/grammar/GrammarBooks.js
@@ -43,49 +43,35 @@ const Lists = styled.div`
   transform: translateX(-50%);
 `;
 
-const CheckCustom = styled(Form.Check)`
+const WrongGrammarSwitch = styled(Form.Check)`
   //크기
-  width: 50px;
+  width: 160px;
+
   //위치
   position: absolute;
   left: 50%;
   transform: translateX(-30%);
-  top: 14%;
+  top: 16%;
 `;
 
-const Bubble = styled.div`
-  //스타일
-  background: #484848;
-  color: white;
-  border-radius: 5px;
-  padding: 7px 12.8px;
+const WriteSwitch = styled(Form.Check)`
   //크기
-  width: 205px;
-  height: 60px;
+  width: 160px;
   //위치
   position: absolute;
-  top: 12%;
-  right: 10px;
-
-  &:after {
-    content: "";
-    position: absolute;
-    left: 10px;
-    top: 50%;
-    width: 0;
-    height: 0;
-    border: 20px solid transparent;
-    border-right-color: #484848;
-    border-left: 0;
-    margin-top: -20px;
-    margin-left: -20px;
-  }
+  left: 50%;
+  transform: translateX(-30%);
+  top: 11%;
 `;
 
-const GrammarBooks = ({ grammarBooks, isLoging, onWrongGrammarSwitch }) => {
+const GrammarBooks = ({
+  grammarBooks,
+  isLoging,
+  onWrongGrammarSwitch,
+  onWriteSwitch,
+}) => {
   return (
     <>
-      <Bubble>버튼을 클릭하면 틀린 문제만 풀 수 있어요!</Bubble>
       <Lists>
         {grammarBooks.map((grammarBook) => (
           <ListItem key={grammarBook.id} size="lg">
@@ -96,12 +82,19 @@ const GrammarBooks = ({ grammarBooks, isLoging, onWrongGrammarSwitch }) => {
         ))}
       </Lists>
       {isLoging && (
-        <CheckCustom
+        <WrongGrammarSwitch
           type="switch"
           id="custom-switch"
+          label="틀린 문제만 풀기"
           onClick={onWrongGrammarSwitch}
         />
       )}
+      <WriteSwitch
+        type="switch"
+        id="custom-switch"
+        label="주관식으로 풀기"
+        onClick={onWriteSwitch}
+      />
     </>
   );
 };
