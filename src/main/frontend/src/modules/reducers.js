db14bd81ce00ce82868fd3c5c61e16a1300e59db
AJN <buzz781@gmail.com>
2024-03-15 17:48:51 +0900

    chore: saga 및 리듀서 연결

--- src/main/frontend/src/modules/reducers.js
+++ src/main/frontend/src/modules/reducers.js
@@ -3,12 +3,14 @@ import auth from "./auth";
 import popup from "./popup";
 import vocaBook from "./voca/vocaBook";
 import voca from "./voca/voca";
+import userLearnPerformance from "./userLearnPerformance/userLearnPerformance";
 
 const rootReducer = combineReducers({
   voca,
   vocaBook,
   auth,
   popup,
+  userLearnPerformance
 });
 
 export default rootReducer;
--- src/main/frontend/src/modules/rootSaga.js
+++ src/main/frontend/src/modules/rootSaga.js
@@ -2,11 +2,12 @@ import createSagaMiddleware from "redux-saga";
 import { all, call } from "redux-saga/effects";
 import vocaBookSaga from "./voca/vocaBookSaga";
 import vocaSaga from "./voca/vocaSaga";
+import userLearnPerformanceSaga from "./userLearnPerformance/userLearnPerformanceSaga";
 
 const sagaMiddleware = createSagaMiddleware();
 
 export function* rootSaga() {
-  yield all([call(vocaSaga), call(vocaBookSaga)]);
+  yield all([call(vocaSaga), call(vocaBookSaga),call(userLearnPerformanceSaga)]);
 }
 
 export default sagaMiddleware;
