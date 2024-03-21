bd59db1533efe34a7594bdb4225acc6f2ec4bc17
AJN <buzz781@gmail.com>
2024-03-22 03:56:36 +0900

    fix: SimplePieChart 수정 (원형모양=>도넛모양)

--- src/main/frontend/src/components/SimplePieChart.js
+++ src/main/frontend/src/components/SimplePieChart.js
@@ -2,11 +2,6 @@ import React from 'react';
 import { PieChart, Pie, Sector, Cell, ResponsiveContainer } from 'recharts';
 import styled from 'styled-components';
 
-
-// const AnimatedContainer = styled.div`
-//   transition: 0.25s all ease-in-out;
-// `
-
 const COLORS = ['#0d6efd','#dd3546'];
 
 const RADIAN = Math.PI / 180;
@@ -24,27 +19,26 @@ const renderCustomizedLabel = ({ cx, cy, midAngle, innerRadius, outerRadius, per
 
 const SimplePieChart = ({data}) => {
   return (
-    // <AnimatedContainer>
-      <ResponsiveContainer width="100%" height={230}>
-        <PieChart width={780} height={780}>
-          <Pie
-            data={data}
-            cx="50%"
-            cy="50%"
-            labelLine={false}
-            label={renderCustomizedLabel}
-            outerRadius={80}
-            fill="#8884d8"
-            dataKey="value"
-            nameKey="name" // name 키를 사용하게 설정
-          >
-            {data.map((entry, index) => (
-              <Cell key={`cell-${index}`} fill={COLORS[index % COLORS.length]} />
-            ))}
-          </Pie>
-        </PieChart>
-      </ResponsiveContainer>
-    // </AnimatedContainer>
+    <ResponsiveContainer width="100%" height={230}>
+      <PieChart width={780} height={780}>
+        <Pie
+          data={data}
+          cx="50%"
+          cy="50%"
+          labelLine={false}
+          label={renderCustomizedLabel}
+          outerRadius={80} // 도넛의 바깥쪽 반지름
+          innerRadius={40} // 도넛의 안쪽 반지름
+          fill="#8884d8"
+          dataKey="value"
+          nameKey="name"
+        >
+          {data.map((entry, index) => (
+            <Cell key={`cell-${index}`} fill={COLORS[index % COLORS.length]} />
+          ))}
+        </Pie>
+      </PieChart>
+    </ResponsiveContainer>
   );
 };
 
