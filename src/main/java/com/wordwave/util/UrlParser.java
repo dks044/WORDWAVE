8b7d8ae16d4c435015c5d52762396a07908fca34
AJN <buzz781@gmail.com>
2024-04-16 11:03:30 +0900

    feat: url 파싱 클래스 추가

---
+++ src/main/java/com/wordwave/util/UrlParser.java
@@ -0,0 +1,11 @@
+package com.wordwave.util;
+
+import java.net.URL;
+
+public class UrlParser {
+    public static String getKeyFromUrl(String urlString) throws Exception {
+        URL url = new URL(urlString);
+        String path = url.getPath();
+        return path.substring(1); // 첫 번째 문자인 '/'를 제거
+    }
+}
