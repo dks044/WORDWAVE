a3082eb65e57742d6bbe269bc01e0ba6a0ab0219
AJN <buzz781@gmail.com>
2024-05-09 18:30:38 +0900

    fix: redis 포트지정 및 host지정

--- src/main/java/com/wordwave/redis/RedisConfig.java
+++ src/main/java/com/wordwave/redis/RedisConfig.java
@@ -1,14 +1,30 @@
 package com.wordwave.redis;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.data.redis.connection.RedisConnectionFactory;
+import org.springframework.data.redis.connection.lettuce.LettuceConnectionFactory;
 import org.springframework.data.redis.core.StringRedisTemplate;
+import org.springframework.data.redis.repository.configuration.EnableRedisRepositories;
 import org.springframework.stereotype.Component;
 
 @Component
+@EnableRedisRepositories
 public class RedisConfig {
+    @Value("${spring.data.redis.host}")
+    private String redisHost;
+
+    @Value("${spring.data.redis.port}")
+    private int redisPort;
+	
+    // RedisProperties로 yaml에 저장한 host, post를 연결
     @Bean
-    public StringRedisTemplate stringRedisTemplate(RedisConnectionFactory connectionFactory) {
+    RedisConnectionFactory redisConnectionFactory() {
+        return new LettuceConnectionFactory(redisHost, redisPort);
+    }
+    
+    @Bean
+    StringRedisTemplate stringRedisTemplate(RedisConnectionFactory connectionFactory) {
         return new StringRedisTemplate(connectionFactory);
     }
 }
