ed2e101dfd34b38090591bedde5d1826094752bd
AJN <buzz781@gmail.com>
2024-05-09 18:34:19 +0900

    fix: 어노테이션 변경

--- src/main/java/com/wordwave/redis/RedisConfig.java
+++ src/main/java/com/wordwave/redis/RedisConfig.java
@@ -2,13 +2,14 @@ package com.wordwave.redis;
 
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
 import org.springframework.data.redis.connection.RedisConnectionFactory;
 import org.springframework.data.redis.connection.lettuce.LettuceConnectionFactory;
 import org.springframework.data.redis.core.StringRedisTemplate;
 import org.springframework.data.redis.repository.configuration.EnableRedisRepositories;
 import org.springframework.stereotype.Component;
 
-@Component
+@Configuration
 @EnableRedisRepositories
 public class RedisConfig {
     @Value("${spring.data.redis.host}")
