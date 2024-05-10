85656d3be846fd118fe0ef838f1046f580668c08
AJN <buzz781@gmail.com>
2024-05-10 13:10:45 +0900

    fix: main 함수 충돌 오류 수정

--- src/main/java/com/wordwave/security/SecurityApplication.java
+++ src/main/java/com/wordwave/security/SecurityApplication.java
@@ -4,7 +4,6 @@ import org.apache.catalina.Context;
 import org.apache.catalina.connector.Connector;
 import org.apache.tomcat.util.descriptor.web.SecurityCollection;
 import org.apache.tomcat.util.descriptor.web.SecurityConstraint;
-import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.web.embedded.tomcat.TomcatServletWebServerFactory;
 import org.springframework.boot.web.servlet.server.ServletWebServerFactory;
@@ -14,10 +13,6 @@ import org.springframework.context.annotation.Bean;
 @SpringBootApplication
 public class SecurityApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(SecurityApplication.class, args);
-
-	}
     @Bean
     ServletWebServerFactory servletContainer() {
         // Enable SSL Trafic
