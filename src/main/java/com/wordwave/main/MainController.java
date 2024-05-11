60fb3d722377b4d10db18e089551d58fe800e7c4
AJN <buzz781@gmail.com>
2024-05-11 10:24:31 +0900

    test: https 테스트

--- src/main/java/com/wordwave/main/MainController.java
+++ src/main/java/com/wordwave/main/MainController.java
@@ -5,8 +5,11 @@ import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-@Controller
 @RestController
 public class MainController {
 
+    @GetMapping("/")
+    public ResponseEntity<String> welcome() {
+        return ResponseEntity.ok("Welcome to WordWave API!");
+    }
 }
